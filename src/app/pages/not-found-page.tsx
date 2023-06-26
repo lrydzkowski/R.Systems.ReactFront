e75fac3479b92c13d5b9ce63508e000837c5cd99
Łukasz Rydzkowski <lukasz.rydzkowski@gmail.com>
2023-06-26 20:21:54 +0200

    feat: set error color

--- src/app/pages/not-found-page.tsx
+++ src/app/pages/not-found-page.tsx
@@ -1,8 +1,10 @@
+import "./not-found-page.css";
+
 export default function NotFoundPage() {
   return (
-    <>
+    <div className="not-found-page">
       <h2>Error 404</h2>
       <p>Page not found</p>
-    </>
+    </div>
   );
 }
